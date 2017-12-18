0aad4a772ef85b0c955f2b561988baad7df3bf74
Stefano Sgorlon <gokyo@users.noreply.github.com>
2017-12-18 15:21:26 +0000

    empty line

--- app/uk/gov/hmrc/apinotificationqueue/repository/Message.scala
+++ app/uk/gov/hmrc/apinotificationqueue/repository/Message.scala
@@ -19,5 +19,4 @@ package uk.gov.hmrc.apinotificationqueue.repository
 import java.sql.Date
 import java.util.UUID
 
-
 case class Message(messageId: UUID, headers: Map[String, String], payload: String, dateReceived: Date)
