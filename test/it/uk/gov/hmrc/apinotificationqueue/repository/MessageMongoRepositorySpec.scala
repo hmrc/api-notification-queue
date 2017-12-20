a0c1858ec0232f972e5c80aef54ec909ba63d368
avinder.bahra <avinder.bahra@digital.hmrc.gov.uk>
2017-12-20 14:25:47 +0000

    API-2933: Added repo method to fetch single notification.

--- test/it/uk/gov/hmrc/apinotificationqueue/repository/MessageMongoRepositorySpec.scala
+++ test/it/uk/gov/hmrc/apinotificationqueue/repository/MessageMongoRepositorySpec.scala
@@ -56,28 +56,49 @@ class MessageMongoRepositorySpec extends UnitSpec
     Json.obj("clientId" -> clientId)
   }
 
-  "repository" should {
+  "repository" can {
+    "save" should {
+      "single notification" in {
+        val actualMessage = await(repository.save(clientId1, notification1))
 
-    "save single notification" in {
-      val actualMessage = await(repository.save(clientId1, notification1))
+        collectionSize shouldBe 1
+        actualMessage shouldBe notification1
+        await(repository.collection.find(selector(clientId1)).one[ClientNotification]).get shouldBe client1Notification1
+      }
 
-      collectionSize shouldBe 1
-      actualMessage shouldBe notification1
-      await(repository.collection.find(selector(clientId1)).one[ClientNotification]).get shouldBe client1Notification1
+      "multiple notification for a client" in {
+        await(repository.save(clientId1, notification1))
+        await(repository.save(clientId1, notification2))
+        await(repository.save(clientId2, notification3))
+
+        collectionSize shouldBe 3
+        val foundList = await(repository.collection.find(selector(clientId1)).cursor[ClientNotification]().collect[List](Int.MaxValue, Cursor.FailOnError[List[ClientNotification]]()))
+        foundList.size shouldBe 2
+        foundList should contain(client1Notification1)
+        foundList should contain(client1Notification2)
+      }
     }
 
-    "save multiple notification for a client" in {
-      await(repository.save(clientId1, notification1))
-      await(repository.save(clientId1, notification2))
-      await(repository.save(clientId2, notification3))
+    "fetch by clientId and notificationId" should {
+      "and return a single record when found" in {
+        await(repository.save(clientId1, notification1))
+        await(repository.save(clientId1, notification2))
 
-      collectionSize shouldBe 3
-      val found = await(repository.collection.find(selector(clientId1)).cursor[ClientNotification]().collect[List](Int.MaxValue, Cursor.FailOnError[List[ClientNotification]]()))
-      found.size shouldBe 2
-      found should contain(client1Notification1)
-      found should contain(client1Notification2)
+        val maybeNotification = await(repository.fetch(clientId1, notification1.notificationId))
+
+        maybeNotification.get shouldBe notification1
+      }
+
+      "and return None when not found" in {
+        await(repository.save(clientId1, notification1))
+        await(repository.save(clientId1, notification2))
+        val nonExistentNotificationId = notification3.notificationId
+
+        val maybeNotification = await(repository.fetch(clientId1, nonExistentNotificationId))
+
+        maybeNotification shouldBe None
+      }
     }
-
   }
 
 }
