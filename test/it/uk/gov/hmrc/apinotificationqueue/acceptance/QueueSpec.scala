77bc7af4978dabf091979781b614e7b7ac4f0a38
Stefano Sgorlon <stefano.sgorlon@digital.hmrc.gov.uk>
2017-12-28 12:11:00 +0000

    API-2924: cleaning

--- test/it/uk/gov/hmrc/apinotificationqueue/acceptance/QueueSpec.scala
+++ test/it/uk/gov/hmrc/apinotificationqueue/acceptance/QueueSpec.scala
@@ -7,7 +7,8 @@ import play.api.mvc.{AnyContentAsEmpty, Headers}
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
 
-class QueueSpec extends FeatureSpec with GivenWhenThen with Matchers with GuiceOneAppPerSuite {
+class QueueSpec extends FeatureSpec with GivenWhenThen
+  with Matchers with GuiceOneAppPerSuite {
 
   feature("Post, retrieve and delete a message from the queue") {
     info("As a 3rd Party system")
@@ -18,22 +19,20 @@ class QueueSpec extends FeatureSpec with GivenWhenThen with Matchers with GuiceO
     scenario("3rd party system gets a message previously queued") {
       Given("a message has already been queued")
       val clientId = "aaaa"
-      val xmlBody = <xml>
-        <node>Stuff</node>
-      </xml>
+      val xmlBody = <xml><node>Stuff</node></xml>
       val queueResponse = await(route(app = app, FakeRequest(POST, "/queue", Headers("x-client-id" -> clientId, "content-type" -> "application/xml"), AnyContentAsEmpty).withXmlBody(xmlBody)).value)
       val location = queueResponse.header.headers("Location")
 
       When("you make a GET based on the location header")
-      val result = route(app = app, FakeRequest(GET, location, Headers("x-client-id" -> clientId), AnyContentAsEmpty)).value
+      val result = route(app, FakeRequest(GET, location, Headers("x-client-id" -> clientId), AnyContentAsEmpty)).value
 
       Then("you will receive a 200 response")
       status(result) shouldBe OK
       And("the message will be the same")
       contentAsString(result) shouldBe xmlBody.toString()
 
-      When("you make a delete based on the location header")
-      val deleteResult = route(app = app, FakeRequest(DELETE, location, Headers("x-client-id" -> clientId), AnyContentAsEmpty)).value
+      When("you make a DELETE based on the Location header")
+      val deleteResult = route(app, FakeRequest(DELETE, location, Headers("x-client-id" -> clientId), AnyContentAsEmpty)).value
 
       Then("you will receive a 204 response")
       status(deleteResult) shouldBe NO_CONTENT
